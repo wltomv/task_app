dfcede93d6a4ff42f38093d42cbcc730d33b392e
Walter Oswaldo <oswaldo@MacBook-Pro-de-Walter.local>
2022-07-17 09:25:22 -0600

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+            child: Text(
+          'TASK APPLICATION',
+        )),
+      ),
+    );
+  }
+}
